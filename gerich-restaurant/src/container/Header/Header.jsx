34ce749cfd1adff01b3573546640008acfeef47b
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-01-05 22:03:46 -0600

    bug with the G background, 106:22

--- gerich-restaurant/src/container/Header/Header.jsx
+++ gerich-restaurant/src/container/Header/Header.jsx
@@ -5,14 +5,15 @@ import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div className='app__header app_wrapper section__padding' id="home">
-    <div className="app_wrapper_info">
+  <div className='app__header app__wrapper section__padding' id="home">
+    <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
       <h1 className='app__header-h1'>The Key to Fine Dining</h1>
-      <p className="p__opensans" style={{margin: '2rem 0'}}></p>
+      <p className="p__opensans" style={{margin: '2rem 0'}}>Lorem Ipsu when an unknown printer took a galley of type and scrambled it to makem is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+      <button type="button" className='custom__button'>Explore Menu</button>
     </div>
-    <div className="app_wrapper_img">
-
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header-img" />
     </div>
   </div>
 );
