7c01c497d6b91963062dfd0cfe0927cca191ebf5
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-01-05 21:29:35 -0600

    updated index.js to react 18 format, got a bug in navbar

--- gerich-restaurant/src/index.js
+++ gerich-restaurant/src/index.js
@@ -1,8 +1,23 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>);
 
+
+reportWebVitals();
