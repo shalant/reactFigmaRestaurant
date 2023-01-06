bf5bc51de74c508b169c7f4fc0bf906f63576d50
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-01-05 21:50:16 -0600

    58

--- gerich-restaurant/src/container/Header/Header.jsx
+++ gerich-restaurant/src/container/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app_wrapper section__padding' id="home">
+    <div className="app_wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{margin: '2rem 0'}}></p>
+    </div>
+    <div className="app_wrapper_img">
+
+    </div>
   </div>
 );
 
