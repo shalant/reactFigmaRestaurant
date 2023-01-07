81a5e881471498f101a890d00f4520b629e68825
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-01-06 21:36:15 -0600

    1:25:00

--- gerich-restaurant/src/components/Menuitem/MenuItem.jsx
+++ gerich-restaurant/src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({title, price, tags}) => (
+  <div className='app__menuItem'>
+    <div className="app__menuItem-head">
+      <div className="app__menuItem-name">
+        <p className="p__cormorant" style={{color: '#dcca87'}}>{title}</p>
+      </div>
+    </div>
   </div>
 );
 
