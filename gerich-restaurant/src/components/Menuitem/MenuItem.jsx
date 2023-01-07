81d526484bf37d897c5be754c3127efb4a62ca8a
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-01-06 22:19:52 -0600

    1:35:17, specialmenu needs the titles to be fixed...

--- gerich-restaurant/src/components/Menuitem/MenuItem.jsx
+++ gerich-restaurant/src/components/Menuitem/MenuItem.jsx
@@ -8,6 +8,16 @@ const MenuItem = ({title, price, tags}) => (
       <div className="app__menuItem-name">
         <p className="p__cormorant" style={{color: '#dcca87'}}>{title}</p>
       </div>
+
+      <div className="app__menuItem-dash"/>
+      
+      <div className="app__menuItem-price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+
+    <div className="app__menuItem-sub">
+      <p className="p__opensans" style={{color:'#AAA'}}>{tags}</p>
     </div>
   </div>
 );
